a8768b8dd008413b8cf6034cc55a9ef84cd2e8f9
akachi11 <ogogorchimadika6@gmail.com>
2025-03-02 22:28:09 +0100

    update

--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -9,7 +9,7 @@ export const { POST } = serve<InitialData>(async (context) => {
   const { email, name } = context.requestPayload
 
   await context.run("new-signup", async () => {
-    await fetch(`${process.env.NEXT_PUBLIC_PROD_API_ENDPOINT ?? "http://localhost:3000"}/api/send-email`, {
+    await fetch(`${process.env.NEXT_PUBLIC_PROD_API_ENDPOINT ?? "http://localhost:3000"}/api/register-workflow`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
